7c5ee1d437284f477ad2e42d2f8ff9a5b0e9955c
Admin <admin@MacBook-Pro-de-Diogo.local>
2021-11-18 01:10:25 -0300

    added permissions

--- core/permissions.py
+++ core/permissions.py
@@ -1,35 +1,35 @@
-# import json
-# from rest_framework import permissions
-# from . import models
+import json
+from rest_framework import permissions
+from . import models
 
-# class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwnerOrReadOnly(permissions.BasePermission):
   
-#   def has_object_permission(self, request, view, obj):
-#     # Read permissions are allowed to any request,
-#     # so we'll always allow GET, HEAD or OPTIONS requests.
-#     if request.method in permissions.SAFE_METHODS:
-#         return True
+  def has_object_permission(self, request, view, obj):
+    # Read permissions are allowed to any request,
+    # so we'll always allow GET, HEAD or OPTIONS requests.
+    if request.method in permissions.SAFE_METHODS:
+        return True
 
-#     # Write permissions are only allowed to the owner of the car.
-#     return obj.owner == request.user
+    # Write permissions are only allowed to the owner of the car.
+    return obj.owner == request.user
 
-# class CarOwnerOrReadOnly(permissions.BasePermission):
+class CarOwnerOrReadOnly(permissions.BasePermission):
   
-#   def has_object_permission(self, request, view, obj):
-#     # Read permissions are allowed to any request,
-#     # so we'll always allow GET, HEAD or OPTIONS requests.
-#     if request.method in permissions.SAFE_METHODS:
-#         return True
+  def has_object_permission(self, request, view, obj):
+    # Read permissions are allowed to any request,
+    # so we'll always allow GET, HEAD or OPTIONS requests.
+    if request.method in permissions.SAFE_METHODS:
+        return True
 
-#     # Write permissions are only allowed to the owner of the car.
-#     return obj.car.owner == request.user
+    # Write permissions are only allowed to the owner of the car.
+    return obj.car.owner == request.user
 
-#   def has_permission(self, request, view):
-#     try:
-#       if request.method == "POST":
-#         # For create action
-#         data = json.loads(request.body)
-#         models.Car.objects.get(pk=data["car"], owner_id=request.user.id)
-#       return True
-#     except:
-#       return False
+  def has_permission(self, request, view):
+    try:
+      if request.method == "POST":
+        # For create action
+        data = json.loads(request.body)
+        models.Car.objects.get(pk=data["car"], owner_id=request.user.id)
+      return True
+    except:
+      return False
